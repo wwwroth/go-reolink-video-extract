4dd70838c2ef14eb62bf20ff3fadef91ab5a7bfa
Phillip Roth <philliproth@gmail.com>
2022-08-11 21:28:09 -0400

    Moving getToken into a function of loginRequest

--- main.go
+++ main.go
@@ -3,20 +3,22 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"strings"
 )
 
-type Cameras []struct {
+type Cameras []Camera
+
+type Camera struct {
 	IP       string `json:"ip"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
-type LoginPayload struct {
+type LoginRequest struct {
 	Cmd   string `json:"cmd"`
 	Param LoginPayloadParam `json:"param"`
 }
@@ -30,6 +32,31 @@ type LoginPayloadUser struct {
 	Password string `json:"password"`
 }
 
+func (payload LoginRequest) getToken(camera Camera) string {
+	json, err := json.Marshal(payload)
+	requestJsonString := fmt.Sprint("[", string(json), "]")
+
+	urlString := fmt.Sprint("http://", camera.IP, "/cgi-bin/api.cgi?cmd=Login")
+
+	client := &http.Client{}
+	var data = strings.NewReader(requestJsonString)
+	req, err := http.NewRequest("POST", urlString, data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	bodyText, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(bodyText)
+}
+
 func main() {
 	file, _ := os.ReadFile("config-cameras.json")
 
@@ -39,7 +66,7 @@ func main() {
 	for i := 0; i < len(cameras); i++ {
 		camera := cameras[i]
 
-		loginPayload := LoginPayload{
+		loginRequest := LoginRequest{
 			Cmd: "Login",
 			Param: LoginPayloadParam{
 				User: LoginPayloadUser{
@@ -49,30 +76,8 @@ func main() {
 			},
 		}
 
-		fmt.Println(loginPayload)
+		token := loginRequest.getToken(camera)
 
-		json, err := json.Marshal(loginPayload)
-		requestJsonString := fmt.Sprint("[", string(json), "]")
-
-		urlString := fmt.Sprint("http://", camera.IP, "/cgi-bin/api.cgi?cmd=Login")
-
-		client := &http.Client{}
-		var data = strings.NewReader(requestJsonString)
-		req, err := http.NewRequest("POST", urlString, data)
-		if err != nil {
-			log.Fatal(err)
-		}
-		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-		resp, err := client.Do(req)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer resp.Body.Close()
-		bodyText, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%s\n", bodyText)
-
+		fmt.Println(token)
 	}
 }
